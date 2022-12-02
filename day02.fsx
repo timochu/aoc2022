617a135406c6da90c1561abfc4790d3a75b8a6de
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-03 00:26:03 +0200

    Formatting day 2

--- day02.fsx
+++ day02.fsx
@@ -9,27 +9,27 @@ let hands = System.IO.File.ReadAllLines "inputs/day02.txt" |> Array.map (fun l -
 hands
 |> Array.sumBy (
     function 
-    | Paper,    Rock        -> 1
-    | Scissors, Paper       -> 2
-    | Rock,     Scissors    -> 3
-    | Rock,     Rock        -> 4
-    | Paper,    Paper       -> 5
-    | Scissors, Scissors    -> 6
-    | Scissors, Rock        -> 7
-    | Rock,     Paper       -> 8
-    | Paper,    Scissors    -> 9)
+    | Paper,    Rock     -> 1
+    | Scissors, Paper    -> 2
+    | Rock,     Scissors -> 3
+    | Rock,     Rock     -> 4
+    | Paper,    Paper    -> 5
+    | Scissors, Scissors -> 6
+    | Scissors, Rock     -> 7
+    | Rock,     Paper    -> 8
+    | Paper,    Scissors -> 9)
 |> printfn "Part 1: %i"
 
 hands
 |> Array.sumBy (
     function 
-    | Paper,    Lose    -> 1
-    | Scissors, Lose    -> 2
-    | Rock,     Lose    -> 3
-    | Rock,     Draw    -> 4
-    | Paper,    Draw    -> 5
-    | Scissors, Draw    -> 6
-    | Scissors, Win     -> 7
-    | Rock,     Win     -> 8
-    | Paper,    Win     -> 9)
+    | Paper,    Lose -> 1
+    | Scissors, Lose -> 2
+    | Rock,     Lose -> 3
+    | Rock,     Draw -> 4
+    | Paper,    Draw -> 5
+    | Scissors, Draw -> 6
+    | Scissors, Win  -> 7
+    | Rock,     Win  -> 8
+    | Paper,    Win  -> 9)
 |> printfn "Part 2: %i"
