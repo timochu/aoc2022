f02aca713eb86a6dc1706e763e3ab1e7464112de
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-03 00:17:36 +0200

    Make day 2 a bit cooler

--- day02.fsx
+++ day02.fsx
@@ -1,85 +1,35 @@
 // puzzle: https://adventofcode.com/2022/day/2
 #time
 
-let hands = System.IO.File.ReadAllLines "inputs/day02.txt" 
+let (|Rock|Paper|Scissors|) = function | 'A' | 'X' -> Rock | 'B' | 'Y' -> Paper | _ -> Scissors
+let (|Win|Draw|Lose|) = function | 'X' -> Lose | 'Y' -> Draw | _ -> Win
 
+let hands = System.IO.File.ReadAllLines "inputs/day02.txt" |> Array.map (fun l -> Seq.head l, Seq.last l)
+    
 hands
 |> Array.sumBy (
-    function
-    | "B X" -> 1
-    | "C Y" -> 2
-    | "A Z" -> 3
-    | "A X" -> 4
-    | "B Y" -> 5
-    | "C Z" -> 6
-    | "C X" -> 7
-    | "A Y" -> 8
-    | "B Z" -> 9
-    | _ -> failwithf "unknown combination")
+    function 
+    | Paper,    Rock        -> 1
+    | Scissors, Paper       -> 2
+    | Rock,     Scissors    -> 3
+    | Rock,     Rock        -> 4
+    | Paper,    Paper       -> 5
+    | Scissors, Scissors    -> 6
+    | Scissors, Rock        -> 7
+    | Rock,     Paper       -> 8
+    | Paper,    Scissors    -> 9)
 |> printfn "Part 1: %i"
 
 hands
 |> Array.sumBy (
-    function
-    | "B X" -> 1
-    | "C X" -> 2
-    | "A X" -> 3
-    | "A Y" -> 4
-    | "B Y" -> 5
-    | "C Y" -> 6
-    | "C Z" -> 7
-    | "A Z" -> 8
-    | "B Z" -> 9
-    | _ -> failwithf "unknown combination")
+    function 
+    | Paper,    Lose    -> 1
+    | Scissors, Lose    -> 2
+    | Rock,     Lose    -> 3
+    | Rock,     Draw    -> 4
+    | Paper,    Draw    -> 5
+    | Scissors, Draw    -> 6
+    | Scissors, Win     -> 7
+    | Rock,     Win     -> 8
+    | Paper,    Win     -> 9)
 |> printfn "Part 2: %i"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let hands3 = 
-    "inputs/day02.txt" 
-    |> System.IO.File.ReadAllLines 
-    |> Array.map (fun s -> Seq.head s, Seq.last s)
-
-hands3
-|> Array.sumBy (
-    function
-    | 'B', 'X' -> 0 + 1 // 1
-    | 'C', 'Y' -> 0 + 2 // 2
-    | 'A', 'Z' -> 0 + 3 // 3
-    | 'A', 'X' -> 3 + 1 // 4
-    | 'B', 'Y' -> 3 + 2 // 5
-    | 'C', 'Z' -> 3 + 3 // 6
-    | 'C', 'X' -> 6 + 1 // 7
-    | 'A', 'Y' -> 6 + 2 // 8
-    | 'B', 'Z' -> 6 + 3 // 9
-    | _ -> failwithf "unknown combination")
-|> printfn "Part 1: %i"
-
-hands3
-|> Array.sumBy (
-    function
-    | 'B', 'X' -> 0 + 1 // 1
-    | 'C', 'X' -> 0 + 2 // 2
-    | 'A', 'X' -> 0 + 3 // 3
-    | 'A', 'Y' -> 3 + 1 // 4
-    | 'B', 'Y' -> 3 + 2 // 5
-    | 'C', 'Y' -> 3 + 3 // 6
-    | 'C', 'Z' -> 6 + 1 // 7
-    | 'A', 'Z' -> 6 + 2 // 8
-    | 'B', 'Z' -> 6 + 3 // 9
-    | _ -> failwithf "unknown combination")
-|> printfn "Part 2: %i"
-
-
