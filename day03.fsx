e2c80d8afca87a2f2a81e8a6e6a635929b3a041e
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-03 10:41:31 +0200

    Day 3 formatting

--- day03.fsx
+++ day03.fsx
@@ -6,11 +6,11 @@ let rucksacks = "inputs/day03.txt" |> System.IO.File.ReadAllLines
 
 rucksacks
 |> Array.sumBy(
-        Seq.map (fun c -> priority |> Seq.findIndex (fun i -> i = c))
-        >> Seq.splitInto 2
-        >> Seq.map set
-        >> (fun x -> Set.intersect (Seq.head x) (Seq.last x))
-        >> Set.maxElement)
+    Seq.map (fun c -> priority |> Seq.findIndex (fun i -> i = c))
+    >> Seq.splitInto 2
+    >> Seq.map set
+    >> (fun x -> Set.intersect (Seq.head x) (Seq.last x))
+    >> Set.maxElement)
 |> printfn "Part 1: %i"
 
 rucksacks
