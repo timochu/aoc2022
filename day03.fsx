9063d902d7b75ea7cb0359598623510e8725b55c
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-03 15:32:11 +0200

    Improve day 3 semantics

--- day03.fsx
+++ day03.fsx
@@ -5,10 +5,10 @@ let priority = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 let rucksacks = "inputs/day03.txt" |> System.IO.File.ReadAllLines
 
 rucksacks
-|> Array.sumBy(Seq.splitInto 2 >> Seq.map set >> Set.intersectMany >> Set.maxElement >> priority.IndexOf)
+|> Array.sumBy(Seq.splitInto 2 >> Seq.map set >> Set.intersectMany >> Seq.head >> priority.IndexOf)
 |> printfn "Part 1: %i"
 
 rucksacks
 |> Array.chunkBySize 3
-|> Array.sumBy(Array.map set >> Set.intersectMany >> Set.maxElement >> priority.IndexOf)
+|> Array.sumBy(Array.map set >> Set.intersectMany >> Seq.head >> priority.IndexOf)
 |> printfn "Part 2: %i"
