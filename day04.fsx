8f390ee5092c86e766044bbd28aa4892f1b2b3b5
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-04 12:10:28 +0200

    Day 4 refactoring

--- day04.fsx
+++ day04.fsx
@@ -1,25 +1,11 @@
 // puzzle: https://adventofcode.com/2022/day/4
 #time
-let split (separator : char[]) (s : string) = s.Split separator
+let pairings = 
+    System.IO.File.ReadAllLines "inputs/day04.txt" 
+    |> Array.map (
+        fun s -> s.Split [|',';'-'|]
+        >> Array.map int
+        >> fun [| a1; a2; b1; b2|] -> [Set [a1..a2]; Set [b1..b2]])
 
-let pairings = System.IO.File.ReadAllLines "inputs/day04.txt"
-
-pairings
-|> Array.where (
-    split [|',';'-'|]
-    >> Array.map int
-    >> fun x -> Set [x.[0] .. x.[1]], Set [x.[2] .. x.[3]]
-    >> fun (x, y) -> Set.isSubset x y || Set.isSubset y x)
-|> Array.length
-|> printfn "Part 1: %i"
-
-pairings
-|> Array.where (
-    split [|',';'-'|]
-    >> Array.map int
-    >> fun x -> [Set [x.[0] .. x.[1]]; Set [x.[2] .. x.[3]]]
-    >> Set.intersectMany
-    >> Seq.isEmpty
-    >> not)
-|> Array.length
-|> printfn "Part 2: %i"
+printfn "Part 1: %A" (pairings |> Array.countBy (fun [ p1; p2 ] -> Set.isSubset p1 p2 || Set.isSubset p2 p1))
+printfn "Part 2: %A" (pairings |> Array.countBy (Set.intersectMany >> Seq.isEmpty >> not))
