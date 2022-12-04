e46195d06644298264c54b827112dad6bb2f808e
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-04 10:39:50 +0200

    First iteration of day 4 part 2

--- day04.fsx
+++ day04.fsx
@@ -10,4 +10,14 @@ let split (separator : char[]) (s : string) = s.Split separator |> List.ofArray
     >> fun x -> Set [x.[0] .. x.[1]], Set [x.[2] .. x.[3]]
     >> fun (x, y) -> Set.isSubset x y || Set.isSubset y x)
 |> Array.length
-|> printfn "%A"
+|> printfn "Part 1: %i"
+
+"inputs/day04.txt"
+|> System.IO.File.ReadAllLines
+|> Array.where (
+    split [|',';'-'|]
+    >> List.map int
+    >> fun x -> Set [x.[0] .. x.[1]], Set [x.[2] .. x.[3]]
+    >> fun (x, y) -> Set.intersect x y |> Seq.isEmpty |> not)
+|> Array.length
+|> printfn "Part 2: %i"
