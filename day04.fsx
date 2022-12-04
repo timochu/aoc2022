1b66c03bc04709b2ec3f92b3310774af7975ca49
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-04 12:53:07 +0200

    Day 4 formatting

--- day04.fsx
+++ day04.fsx
@@ -3,7 +3,10 @@
 
 let pairings = 
     System.IO.File.ReadAllLines "inputs/day04.txt" 
-    |> Array.map ((fun s -> s.Split(',', '-')) >> Array.map int >> fun [| a1; a2; b1; b2 |] -> Set [a1..a2], Set [b1..b2])
+    |> Array.map (
+        fun s -> s.Split(',', '-')
+        >> Array.map int 
+        >> fun [| a1; a2; b1; b2 |] -> Set [a1..a2], Set [b1..b2])
 
-printfn "Part 1: %A" (pairings |> Array.countBy ((fun (p1, p2) -> Set.isSubset p1 p2 || Set.isSubset p2 p1)))
-printfn "Part 2: %A" (pairings |> Array.countBy ((fun (p1, p2) -> Set.intersect p1 p2) >> Seq.isEmpty >> not))
+printfn "Part 1: %A" (pairings |> Array.countBy (fun (p1, p2) -> Set.isSubset p1 p2 || Set.isSubset p2 p1))
+printfn "Part 2: %A" (pairings |> Array.countBy (fun (p1, p2) -> Set.intersect p1 p2) |> Seq.isEmpty |> not)
