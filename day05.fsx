f49e750a076c37de39f0e10f87bb4c76eaf8b959
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-05 15:03:29 +0200

    Clean up day 5

--- day05.fsx
+++ day05.fsx
@@ -1,54 +1,33 @@
-// puzzle: https://adventofcode.com/2022/day/4
+// puzzle: https://adventofcode.com/2022/day/5
 #time
 
 open System
 open System.Collections.Generic
 open type System.StringSplitOptions
 
-module Part1 =
-    let stacksInput, ordersInput = 
-        IO.File.ReadAllLines "inputs/day05.txt"
-        |> fun i -> Array.take 8 i, Array.skip 10 i
+let stacksInput, orders = 
+    IO.File.ReadAllLines "inputs/day05.txt"
+    |> fun i -> Array.take 8 i, Array.skip 10 i |> Array.map (
+        fun x -> x.Split([|"move "; " from "; " to "|], RemoveEmptyEntries)
+        >> Array.map (int >> (fun x -> x-1)) >> (fun x -> x.[0], x.[1], x.[2]))
 
-    let stacks = 
-        [| for column in 1 .. +4 .. 33 -> 
-            [| for row in 7 .. -1 .. 0 -> stacksInput.[row].[column] |]
-            |> Seq.where ((=) ' ' >> not)
-            |> Stack |]
+let stacks = 
+    [| for column in 1 .. +4 .. 33 -> 
+        [| for row in 7 .. -1 .. 0 -> stacksInput.[row].[column] |]
+        |> Seq.where ((=) ' ' >> not)
+        |> Stack |]
 
-    let orders =
-        ordersInput
-        |> Array.map (
-            fun x -> x.Split([|"move "; " from "; " to "|], RemoveEmptyEntries)
-            >> Array.map (int >> (fun x -> x-1)) >> (fun x -> x.[0], x.[1], x.[2])) 
+let stacks2 = stacks |> Array.map(Stack >> Stack)
 
-    orders
-    |> Array.map (fun (count, from, destination) ->
-        [| for _ in 0 .. count -> stacks.[from].Pop() |> stacks.[destination].Push |])
+orders
+|> Array.map (fun (count, from, destination) ->
+    [| for _ in 0 .. count -> stacks.[from].Pop() |> stacks.[destination].Push |])
 
-    printfn "Part 1: %s" (stacks |> Array.map (fun x -> x.Peek()) |> String)
+orders
+|> Array.map (fun (count, from, destination) ->
+    [| for _ in 0 .. count -> stacks2.[from].Pop() |] 
+    |> Array.rev 
+    |> Array.iter stacks2.[destination].Push )
 
-module Part2 =
-    let stacksInput, ordersInput = 
-        IO.File.ReadAllLines "inputs/day05.txt"
-        |> fun i -> Array.take 8 i, Array.skip 10 i
-
-    let stacks = 
-        [| for column in 1 .. +4 .. 33 -> 
-            [| for row in 7 .. -1 .. 0 -> stacksInput.[row].[column] |]
-            |> Seq.where ((=) ' ' >> not)
-            |> Stack |]
-
-    let orders =
-        ordersInput
-        |> Array.map (
-            fun x -> x.Split([|"move "; " from "; " to "|], RemoveEmptyEntries)
-            >> Array.map (int >> (fun x -> x-1)) >> (fun x -> x.[0], x.[1], x.[2])) 
-
-    orders
-    |> Array.map (fun (count, from, destination) ->
-        [| for _ in 0 .. count -> stacks.[from].Pop() |] 
-        |> Array.rev 
-        |> Array.iter stacks.[destination].Push )
-
-    printfn "Part 2: %s" (stacks |> Array.map (fun x -> x.Peek()) |> String)
+printfn "Part 1: %s" (stacks |> Array.map (fun x -> x.Peek()) |> String)
+printfn "Part 2: %s" (stacks2 |> Array.map (fun x -> x.Peek()) |> String)
