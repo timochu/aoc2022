5971b29abc3b68b53b9e332fc3467e5730a49c5b
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-05 15:38:53 +0200

    Day 5 golf

--- day05.fsx
+++ day05.fsx
@@ -3,31 +3,24 @@
 
 open System
 open System.Collections.Generic
-open type System.StringSplitOptions
 
 let stacksInput, orders = 
     IO.File.ReadAllLines "inputs/day05.txt"
     |> fun i -> Array.take 8 i, Array.skip 10 i |> Array.map (
-        fun x -> x.Split([|"move "; " from "; " to "|], RemoveEmptyEntries)
+        fun x -> x.Split([|"move "; " from "; " to "|], StringSplitOptions.RemoveEmptyEntries)
         >> Array.map (int >> (fun x -> x-1)) >> (fun x -> x.[0], x.[1], x.[2]))
 
-let part1Stacks, part2Stacks = 
-    [| for column in 1 .. +4 .. 33 -> 
+let s1, s2 =
+    [| for column in 1 .. +4 .. 33 ->
         [| for row in 7 .. -1 .. 0 -> stacksInput.[row].[column] |]
         |> Seq.where ((=) ' ' >> not)
-        |> fun s -> Stack s, Stack s |] 
+        |> fun s -> Stack s, Stack s |]
     |> Array.unzip
 
 orders
-|> Array.iter (fun (count, from, destination) ->
-    [| for _ in 0 .. count -> part1Stacks.[from].Pop() |]
-    |> Array.iter part1Stacks.[destination].Push)
+|> Array.iter (fun (c, f, t) ->
+    [| for _ in 0 .. c -> s1.[f].Pop() |] |> Array.iter s1.[t].Push
+    [| for _ in 0 .. c -> s2.[f].Pop() |] |> Array.rev |> Array.iter s2.[t].Push)
 
-orders
-|> Array.iter (fun (count, from, destination) ->
-    [| for _ in 0 .. count -> part2Stacks.[from].Pop() |] 
-    |> Array.rev 
-    |> Array.iter part2Stacks.[destination].Push )
-
-printfn "Part 1: %s" (part1Stacks |> Array.map (fun x -> x.Peek()) |> String)
-printfn "Part 2: %s" (part2Stacks |> Array.map (fun x -> x.Peek()) |> String)
+printfn "Part 1: %s" (s1 |> Array.map (fun x -> x.Peek()) |> String)
+printfn "Part 2: %s" (s2 |> Array.map (fun x -> x.Peek()) |> String)
