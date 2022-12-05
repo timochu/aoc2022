68f033655cc2e32ec3431e70b0756508295e27de
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-05 15:31:59 +0200

    Day 5 formatting

--- day05.fsx
+++ day05.fsx
@@ -11,23 +11,23 @@ let stacksInput, orders =
         fun x -> x.Split([|"move "; " from "; " to "|], RemoveEmptyEntries)
         >> Array.map (int >> (fun x -> x-1)) >> (fun x -> x.[0], x.[1], x.[2]))
 
-let stacks = 
+let part1Stacks, part2Stacks = 
     [| for column in 1 .. +4 .. 33 -> 
         [| for row in 7 .. -1 .. 0 -> stacksInput.[row].[column] |]
         |> Seq.where ((=) ' ' >> not)
-        |> Stack |]
-
-let stacks2 = stacks |> Array.map(Stack >> Stack)
-
-orders
-|> Array.map (fun (count, from, destination) ->
-    [| for _ in 0 .. count -> stacks.[from].Pop() |> stacks.[destination].Push |])
+        |> fun s -> Stack s, Stack s |] 
+    |> Array.unzip
 
 orders
-|> Array.map (fun (count, from, destination) ->
-    [| for _ in 0 .. count -> stacks2.[from].Pop() |] 
+|> Array.iter (fun (count, from, destination) ->
+    [| for _ in 0 .. count -> part1Stacks.[from].Pop() |]
+    |> Array.iter part1Stacks.[destination].Push)
+
+orders
+|> Array.iter (fun (count, from, destination) ->
+    [| for _ in 0 .. count -> part2Stacks.[from].Pop() |] 
     |> Array.rev 
-    |> Array.iter stacks2.[destination].Push )
+    |> Array.iter part2Stacks.[destination].Push )
 
-printfn "Part 1: %s" (stacks |> Array.map (fun x -> x.Peek()) |> String)
-printfn "Part 2: %s" (stacks2 |> Array.map (fun x -> x.Peek()) |> String)
+printfn "Part 1: %s" (part1Stacks |> Array.map (fun x -> x.Peek()) |> String)
+printfn "Part 2: %s" (part2Stacks |> Array.map (fun x -> x.Peek()) |> String)
