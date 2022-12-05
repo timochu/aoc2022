55f178ea0b3254eceec6ae2f487453cc07d5a673
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-05 16:20:08 +0200

    Day 5 formatting

--- day05.fsx
+++ day05.fsx
@@ -2,12 +2,9 @@ open System
 open System.Collections.Generic
 
 let input = IO.File.ReadAllLines "inputs/day05.txt" 
-let orders = 
-    input
-    |> Array.skip 10 
-    |> Array.map (
-        fun x -> x.Split([|"move "; " from "; " to "|], StringSplitOptions.RemoveEmptyEntries)
-        >> Array.map (int >> (fun x -> x-1)) >> (fun x -> x.[0], x.[1], x.[2]))
+let orders = input |> Array.skip 10 |> Array.map (
+    fun x -> x.Split([|"move "; " from "; " to "|], StringSplitOptions.RemoveEmptyEntries)
+    >> Array.map (int >> (fun x -> x-1)) >> (fun x -> x.[0], x.[1], x.[2]))
 
 let stacks1, stacks2 = 
     let stacks = input |> Array.take 8
@@ -17,8 +14,7 @@ let stacks1, stacks2 =
         |> fun s -> Stack s, Stack s |]
     |> Array.unzip
 
-orders
-|> Array.iter (fun (count, from, ``to``) ->
+orders |> Array.iter (fun (count, from, ``to``) ->
     [| for _ in 0 .. count -> stacks1.[from].Pop() |]              |> Array.iter stacks1.[``to``].Push
     [| for _ in 0 .. count -> stacks2.[from].Pop() |] |> Array.rev |> Array.iter stacks2.[``to``].Push)
 
