04226f1523ff38c07e63a29eb92fc4f00739ddb0
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-05 16:25:19 +0200

    Day 5 cleanup

--- day05.fsx
+++ day05.fsx
@@ -1,7 +1,8 @@
 open System
 open System.Collections.Generic
 
-let input = IO.File.ReadAllLines "inputs/day05.txt" 
+let input = IO.File.ReadAllLines "inputs/day05.txt"
+
 let orders = input |> Array.skip 10 |> Array.map (
     fun x -> x.Split([|"move "; " from "; " to "|], StringSplitOptions.RemoveEmptyEntries)
     >> Array.map (int >> (fun x -> x-1)) >> (fun x -> x.[0], x.[1], x.[2]))
@@ -10,7 +11,7 @@ let stacks1, stacks2 =
     let stacks = input |> Array.take 8
     [| for column in 1 .. +4 .. 33 ->
         [| for row in 7 .. -1 .. 0 -> stacks.[row].[column] |]
-        |> Seq.where ((=) ' ' >> not)
+        |> Seq.where Char.IsLetterOrDigit
         |> fun s -> Stack s, Stack s |]
     |> Array.unzip
 
