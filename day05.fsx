1ed01a8040c348301cec62313134a58591d656ff
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-05 16:19:20 +0200

    Day 5 cleanup

--- day05.fsx
+++ day05.fsx
@@ -1,26 +1,26 @@
-// puzzle: https://adventofcode.com/2022/day/5
-#time
-
 open System
 open System.Collections.Generic
 
-let stacksInput, orders = 
-    IO.File.ReadAllLines "inputs/day05.txt"
-    |> fun i -> Array.take 8 i, Array.skip 10 i |> Array.map (
+let input = IO.File.ReadAllLines "inputs/day05.txt" 
+let orders = 
+    input
+    |> Array.skip 10 
+    |> Array.map (
         fun x -> x.Split([|"move "; " from "; " to "|], StringSplitOptions.RemoveEmptyEntries)
         >> Array.map (int >> (fun x -> x-1)) >> (fun x -> x.[0], x.[1], x.[2]))
 
-let s1, s2 =
+let stacks1, stacks2 = 
+    let stacks = input |> Array.take 8
     [| for column in 1 .. +4 .. 33 ->
-        [| for row in 7 .. -1 .. 0 -> stacksInput.[row].[column] |]
+        [| for row in 7 .. -1 .. 0 -> stacks.[row].[column] |]
         |> Seq.where ((=) ' ' >> not)
         |> fun s -> Stack s, Stack s |]
     |> Array.unzip
 
 orders
-|> Array.iter (fun (c, f, t) ->
-    [| for _ in 0 .. c -> s1.[f].Pop() |] |> Array.iter s1.[t].Push
-    [| for _ in 0 .. c -> s2.[f].Pop() |] |> Array.rev |> Array.iter s2.[t].Push)
+|> Array.iter (fun (count, from, ``to``) ->
+    [| for _ in 0 .. count -> stacks1.[from].Pop() |]              |> Array.iter stacks1.[``to``].Push
+    [| for _ in 0 .. count -> stacks2.[from].Pop() |] |> Array.rev |> Array.iter stacks2.[``to``].Push)
 
-printfn "Part 1: %s" (s1 |> Array.map (fun x -> x.Peek()) |> String)
-printfn "Part 2: %s" (s2 |> Array.map (fun x -> x.Peek()) |> String)
+printfn "Part 1: %s" (stacks1 |> Array.map (fun x -> x.Peek()) |> String)
+printfn "Part 2: %s" (stacks2 |> Array.map (fun x -> x.Peek()) |> String)
