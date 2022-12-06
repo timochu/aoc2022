1a27f849dab629b71fb806cd73337e3bb234f577
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-06 10:21:00 +0200

    Day 6 naming

--- day06.fsx
+++ day06.fsx
@@ -1,3 +1,3 @@
-let input = System.IO.File.ReadAllText "inputs/day06.txt"
+let signal = System.IO.File.ReadAllText "inputs/day06.txt"
 let find n = Seq.windowed n >> Seq.map set >> Seq.findIndex (fun s -> s.Count = n) >> (+) n
-printfn "Part 1: %i\nPart 2: %i" (input |> find 4) (input |> find 14)
+printfn "Part 1: %i\nPart 2: %i" (signal |> find 4) (signal |> find 14)
