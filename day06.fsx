0b15d94548a4f0e73ed5c8a37ff6491d4307861a
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-06 10:09:15 +0200

    Day 6 part 2

--- day06.fsx
+++ day06.fsx
@@ -4,3 +4,10 @@ System.IO.File.ReadAllText "inputs/day06.txt"
 |> Seq.findIndex (fun s -> s.Count = 4)
 |> (+) 4
 |> printfn "Part 1: %i"
+
+System.IO.File.ReadAllText "inputs/day06.txt"
+|> Seq.windowed 14
+|> Seq.map set
+|> Seq.findIndex (fun s -> s.Count = 14)
+|> (+) 14
+|> printfn "Part 2: %i"
