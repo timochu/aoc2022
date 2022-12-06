2af23bba818a0973b6e8633dcfe4ff0a494d34c7
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-06 11:50:58 +0200

    Day 6 change lambda

--- day06.fsx
+++ day06.fsx
@@ -1,3 +1,3 @@
 let signal = System.IO.File.ReadAllText "inputs/day06.txt"
-let find n = Seq.windowed n >> Seq.map set >> Seq.findIndex (fun s -> s.Count = n) >> (+) n
+let find n = Seq.windowed n >> Seq.map set >> Seq.findIndex (Set.count >> (=) n) >> (+) n
 printfn "Part 1: %i & Part 2: %i" (signal |> find 4) (signal |> find 14)
