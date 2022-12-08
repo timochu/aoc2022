242cf6e752797793a2d65579c730145e9c51317d
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-09 00:27:29 +0200

    Day 7 cleanup

--- day07.fsx
+++ day07.fsx
@@ -1,13 +1,13 @@
 let rec calculateSizes (path : string) files lines = 
-    if lines |> Array.isEmpty then 
+    if lines |> Array.isEmpty  then 
         files 
         |> List.groupBy fst 
         |> List.map (fun (path, _) -> path, files |> List.where (fun (p : string, _) -> p.StartsWith path) |> List.sumBy snd)
     else
         match lines |> Array.head with
         | "$ cd .."                                -> path.Substring(0, path |> Seq.findIndexBack ((=) '/')), files
-        | l when l.StartsWith "$ cd" && path = "/" -> $"/{l.Substring(5)}", files
-        | l when l.StartsWith "$ cd"               -> $"{path}/{l.Substring(5)}", files
+        | l when l.StartsWith "$ cd" && path = "/" -> $"/{l.Substring 5}", files
+        | l when l.StartsWith "$ cd"               -> $"{path}/{l.Substring 5}", files
         | l when l.[0] |> System.Char.IsDigit      -> path, (path, l |> fun (i:string) -> i.Split " " |> Seq.head |> int) :: files
         | l when l.StartsWith "dir"                -> path, (path, 0) :: files
         | _                                        -> path, files
