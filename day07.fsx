28fa575c4459af9e50005765d9d406c0c3e4cdb3
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-09 00:22:50 +0200

    Day 7 cleanup

--- day07.fsx
+++ day07.fsx
@@ -4,15 +4,14 @@ let rec calculateSizes (path : string) files lines =
         |> List.groupBy fst 
         |> List.map (fun (path, _) -> path, files |> List.where (fun (p : string, _) -> p.StartsWith path) |> List.sumBy snd)
     else
-        let path, f =
-            match lines |> Array.head with
-            | "$ cd .."                                -> path.Substring(0, path |> Seq.findIndexBack ((=) '/')), files
-            | l when l.StartsWith "$ cd" && path = "/" -> $"/{l.Substring(5)}", files
-            | l when l.StartsWith "$ cd"               -> $"{path}/{l.Substring(5)}", files
-            | l when l.[0] |> System.Char.IsDigit      -> path, (path, l |> fun (i:string) -> i.Split " " |> Seq.head |> int) :: files
-            | l when l.StartsWith "dir"                -> path, (path, 0) :: files
-            | _ -> path, files
-        calculateSizes path f (lines |> Array.tail)
+        match lines |> Array.head with
+        | "$ cd .."                                -> path.Substring(0, path |> Seq.findIndexBack ((=) '/')), files
+        | l when l.StartsWith "$ cd" && path = "/" -> $"/{l.Substring(5)}", files
+        | l when l.StartsWith "$ cd"               -> $"{path}/{l.Substring(5)}", files
+        | l when l.[0] |> System.Char.IsDigit      -> path, (path, l |> fun (i:string) -> i.Split " " |> Seq.head |> int) :: files
+        | l when l.StartsWith "dir"                -> path, (path, 0) :: files
+        | _ -> path, files
+        ||> calculateSizes <| Array.tail lines
 
 let sizes = System.IO.File.ReadAllLines "inputs/day07.txt" |> Array.skip 1 |> calculateSizes "/" []
 let spaceNeeded = sizes |> List.find (fst >> (=) "/") |> fun (_, spaceUsed) -> 30000000 - (70000000 - spaceUsed)
