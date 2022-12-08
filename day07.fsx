6db375090c77cd827be3ba6c6a6c7ea4b19e75eb
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-08 23:40:16 +0200

    Day 7 cleanup

--- day07.fsx
+++ day07.fsx
@@ -1,4 +1,4 @@
-let rec calculateSizes lines (path : string) files = 
+let rec calculateSizes (path : string) files lines = 
     if lines |> Array.isEmpty then 
         files |> List.groupBy fst |> List.map (fun (x,_) -> x, files |> List.sumBy (fun ((pp : string), size) -> if pp.StartsWith(x) then size else 0))
     else
@@ -18,9 +18,10 @@ let rec calculateSizes lines (path : string) files =
                 files @ [(path, 0)]
             else files
 
-        calculateSizes (lines |> Array.tail) path f
+        calculateSizes path f (lines |> Array.tail)
 
-let sizes = calculateSizes (System.IO.File.ReadAllLines "inputs/day07.txt" |> Array.skip 1) "/" []
+let sizes = System.IO.File.ReadAllLines "inputs/day07.txt" |> Array.skip 1 |> calculateSizes "/" []
 let spaceNeeded = sizes |> List.find (fst >> (=) "/") |> fun (_, spaceUsed) -> 30000000 - (70000000 - spaceUsed)
+
 printfn "Part 1: %i" (sizes |> List.where (snd >> (>=) 100000) |> List.sumBy snd)
 printfn "Part 2: %A" (sizes |> List.sortBy snd |> List.find (snd >> (<) spaceNeeded))
