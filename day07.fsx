520ce720d665caa4457a3b4eede03483ebc00177
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-08 23:51:04 +0200

    Day 7 cleanup

--- day07.fsx
+++ day07.fsx
@@ -1,15 +1,17 @@
-let rec calculateSizes (path : string) files lines = 
+let rec calculateSizes (currentPath : string) files lines = 
     if lines |> Array.isEmpty then 
-        files |> List.groupBy fst |> List.map (fun (x,_) -> x, files |> List.sumBy (fun ((pp : string), size) -> if pp.StartsWith(x) then size else 0))
+        files 
+        |> List.groupBy fst 
+        |> List.map (fun (path, _) -> path, files |> List.sumBy (fun (p : string, size) -> if p.StartsWith path then size else 0))
     else
         let path, f =
             match lines |> Array.head with
-            | "$ cd .." -> path.Substring(0, path |> Seq.findIndexBack ((=) '/')), files
-            | line when line.StartsWith("$ cd") && path = "/" -> $"/{line.Substring(5)}", files
-            | line when line.StartsWith("$ cd") -> $"{path}/{line.Substring(5)}", files
-            | line when line |> Seq.head |> System.Char.IsDigit -> path,  (path, line |> Seq.takeWhile System.Char.IsDigit |> Seq.toArray |> System.String |> int) :: files
-            | line when line.StartsWith("dir") -> path, (path, 0) :: files
-            | _ -> path, files
+            | "$ cd .." -> currentPath.Substring(0, currentPath |> Seq.findIndexBack ((=) '/')), files
+            | line when line.StartsWith "$ cd" && currentPath = "/" -> $"/{line.Substring(5)}", files
+            | line when line.StartsWith "$ cd" -> $"{currentPath}/{line.Substring(5)}", files
+            | line when line |> Seq.head |> System.Char.IsDigit -> currentPath,  (currentPath, line |> Seq.takeWhile System.Char.IsDigit |> Seq.toArray |> System.String |> int) :: files
+            | line when line.StartsWith "dir" -> currentPath, (currentPath, 0) :: files
+            | _ -> currentPath, files
         calculateSizes path f (lines |> Array.tail)
 
 let sizes = System.IO.File.ReadAllLines "inputs/day07.txt" |> Array.skip 1 |> calculateSizes "/" []
