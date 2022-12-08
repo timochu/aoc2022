ef94c0b982f0a42aec57336f6a7766eba6ef2aa3
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-08 22:39:30 +0200

    Day 7 cleanup

--- day07.fsx
+++ day07.fsx
@@ -1,20 +1,18 @@
 let output = System.IO.File.ReadAllLines "inputs/day07.txt"
 
-let rec calculateSizes (lines : string[]) (currentPath : string) (files : List<string * int>) =
-    if lines |> Array.length = 0 then files
+let rec calculateSizes lines (path : string) files =
+    if lines |> Array.isEmpty then files
     else
         let line = lines |> Array.head
         let path =
             match line with
             | "$ cd /" -> "/"
-            | "$ cd .." -> currentPath.Substring(0, currentPath |> Seq.findIndexBack ((=) '/'))
-            | c when c.StartsWith("$ cd") -> 
-                if currentPath = "/" then $"/{c.Substring(5)}"
-                else $"{currentPath}/{c.Substring(5)}"
-            | _ -> currentPath
-        printfn "currentPath %s" path
+            | "$ cd .." -> path.Substring(0, path |> Seq.findIndexBack ((=) '/'))
+            | c when c.StartsWith("$ cd") && path = "/" -> $"/{c.Substring(5)}"
+            | c when c.StartsWith("$ cd") -> $"{path}/{c.Substring(5)}"
+            | _ -> path
         
-        let foo = 
+        let f = 
             if line |> Seq.head |> System.Char.IsDigit then
                 let size = line |> Seq.takeWhile System.Char.IsDigit |> Seq.toArray |> System.String |> int
                 files @ [(path, size)]
@@ -22,21 +20,10 @@ let rec calculateSizes (lines : string[]) (currentPath : string) (files : List<s
                 files @ [(path, 0)]
             else files
 
-        calculateSizes (lines |> Array.tail) path foo
+        calculateSizes (lines |> Array.tail) path f
 
 let result = calculateSizes output "" List.empty
-printfn "\n\n----------\n\n"
 
-result |> List.iter (printfn "%A")
-printfn "\n\n----------\n\n"
-result 
-|> List.groupBy fst 
-|> List.map (fun (x,y) -> x, result |> List.where (fun (path, _) -> path.StartsWith(x)) |> List.sumBy snd) 
-// |> List.where (fun (_, y) -> y <= 100000) 
-// |> List.sumBy snd
-|> List.iter (printfn "%A")
-
-printfn "\n\n----------\n\n"
 result 
 |> List.groupBy fst 
 |> List.map (fun (x,y) -> x, result |> List.where (fun (path, _) -> path.StartsWith(x)) |> List.sumBy snd) 
