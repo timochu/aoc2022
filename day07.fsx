f0d40f8846358be851ac5f23ab1aaf04b4293e6f
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-08 23:32:05 +0200

    Day 7 cleanup

--- day07.fsx
+++ day07.fsx
@@ -1,13 +1,10 @@
-let rec calculateSizes lines (path : string) files =
+let rec calculateSizes lines (path : string) files = 
     if lines |> Array.isEmpty then 
-        files 
-        |> List.groupBy fst 
-        |> List.map (fun (x,_) -> x, files |> List.where (fun ((pp : string), _) -> pp.StartsWith(x)) |> List.sumBy snd) 
+        files |> List.groupBy fst |> List.map (fun (x,_) -> x, files |> List.sumBy (fun ((pp : string), size) -> if pp.StartsWith(x) then size else 0))
     else
         let line = lines |> Array.head
         let path =
             match line with
-            // | "$ cd /" -> "/"
             | "$ cd .." -> path.Substring(0, path |> Seq.findIndexBack ((=) '/'))
             | c when c.StartsWith("$ cd") && path = "/" -> $"/{c.Substring(5)}"
             | c when c.StartsWith("$ cd") -> $"{path}/{c.Substring(5)}"
@@ -23,9 +20,7 @@ let rec calculateSizes lines (path : string) files =
 
         calculateSizes (lines |> Array.tail) path f
 
-let sizes = calculateSizes (System.IO.File.ReadAllLines "inputs/day07.txt" |> Array.skip 1) "/" List.empty
-
+let sizes = calculateSizes (System.IO.File.ReadAllLines "inputs/day07.txt" |> Array.skip 1) "/" []
 let spaceNeeded = sizes |> List.find (fst >> (=) "/") |> fun (_, spaceUsed) -> 30000000 - (70000000 - spaceUsed)
-
 printfn "Part 1: %i" (sizes |> List.where (snd >> (>=) 100000) |> List.sumBy snd)
 printfn "Part 2: %A" (sizes |> List.sortBy snd |> List.find (snd >> (<) spaceNeeded))
