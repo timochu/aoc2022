ae6e327e18c1b84d5cdc8cd39917bcfacdfc7dba
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-09 23:47:13 +0200

    Day 8 part 1 cleanup

--- day08.fsx
+++ day08.fsx
@@ -1,16 +1,14 @@
 let forest = System.IO.File.ReadAllLines "inputs/day08.txt" |> Array.map (Seq.map (System.Char.ToString >> int) >> Seq.toList) |> List.ofArray
 
-let rec checkVisibility (visibilityMap : bool list) (treeLine : int list) = 
+let rec checkVisibility (visibilityMap : bool list) (treeLine : int list) =
     if visibilityMap.Length = treeLine.Length then 
         visibilityMap |> List.rev
     else 
         let i = visibilityMap.Length
-        let right = treeLine.[ .. i-1] |> List.forall (fun x -> x < treeLine.[i])
-        let left = treeLine.[i+1..] |> List.forall (fun x -> x < treeLine.[i])
+        let right = treeLine.[ .. i-1] |> List.forall ((>) treeLine.[i])
+        let left = treeLine.[i+1..] |> List.forall ((>) treeLine.[i])
         checkVisibility ((right || left) :: visibilityMap) treeLine
 
-let result = forest |> List.map (checkVisibility [])
-let result2 = forest |> List.transpose |> List.map (checkVisibility []) |> List.transpose
-let result3 = (result, result2) ||> List.map2 (fun horizontal vertical -> (horizontal, vertical) ||> List.map2 (fun h v -> h || v))
-
-result3 |> List.sumBy(fun row -> row |> List.where (fun y -> y = true) |> List.length) |> printfn "%A"
+let horizontal = forest |> List.map (checkVisibility [])
+let vertical = forest |> List.transpose |> List.map (checkVisibility []) |> List.transpose
+(horizontal, vertical) ||> List.map2 (List.map2 (||)) |> List.sumBy(List.where id >> List.length) |> printfn "%i"
