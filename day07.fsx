5924bf8c86da2335b5991fe3d9b1afc2c37134be
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-09 13:45:27 +0200

    Day 7 cleanup

--- day07.fsx
+++ day07.fsx
@@ -1,23 +1,20 @@
-open System
-
 let rec calculateSizes lines (path : string) files =
-    match lines with
-    | [] -> 
+    if lines |> Array.isEmpty then
         files 
         |> List.groupBy fst 
         |> List.map (fun (path, _) -> path, files |> List.where (fun (p : string, _) -> p.StartsWith path) |> List.sumBy snd)
         |> Map
-    | head :: tail ->
-        match head with
-        | "$ cd .."                                -> path.Substring(0, path.LastIndexOf "/"), files
-        | l when l.StartsWith "$ cd" && path = "/" -> $"/{l.Substring 5}", files
-        | l when l.StartsWith "$ cd"               -> $"{path}/{l.Substring 5}", files
-        | l when l.[0] |> Char.IsDigit             -> path, (path, l |> Seq.takeWhile Char.IsDigit |> Array.ofSeq |> String |> int) :: files
-        | _                                        -> path, (path, 0) :: files
-        ||> calculateSizes tail
+    else
+        match lines |> Array.head with
+        | "$ ls" -> path, files
+        | "$ cd .."                                      -> path.Remove(path.LastIndexOf "/"), files
+        | line when line.StartsWith "$ cd" && path = "/" -> $"/{line.Substring 5}", files
+        | line when line.StartsWith "$ cd" -> $"{path}/{line.Substring 5}", files
+        | line when line.StartsWith "dir" -> path, (path, 0) :: files
+        | file -> path, (path, file |> Seq.takeWhile System.Char.IsDigit |> Array.ofSeq |> System.String |> int) :: files
+        ||> calculateSizes (lines |> Array.tail)
 
-let terminalOutput = IO.File.ReadLines "inputs/day07.txt" |> Seq.tail |> Seq.toList
-let sizes = calculateSizes terminalOutput "/" []
+let sizes = calculateSizes (System.IO.File.ReadAllLines "inputs/day07.txt") "/" []
 let spaceNeeded = sizes |> Map.find "/" |> fun size -> 30000000 - (70000000 - size)
 
 printfn "Part 1: %i" (sizes |> Map.values |> Seq.where ((>=) 100000) |> Seq.sum)
