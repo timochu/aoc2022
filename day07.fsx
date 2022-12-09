1b3000b12eaac17bed3a48eef2497206ef2ee6d2
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-09 11:30:48 +0200

    Day 7 cleanup

--- day07.fsx
+++ day07.fsx
@@ -1,22 +1,24 @@
-let rec calculateSizes lines (path : string) files = 
+open System
+
+let rec calculateSizes lines (path : string) files =
     match lines with
     | [] -> 
         files 
         |> List.groupBy fst 
         |> List.map (fun (path, _) -> path, files |> List.where (fun (p : string, _) -> p.StartsWith path) |> List.sumBy snd)
+        |> Map
     | head :: tail ->
         match head with
         | "$ cd .."                                -> path.Substring(0, path |> Seq.findIndexBack ((=) '/')), files
         | l when l.StartsWith "$ cd" && path = "/" -> $"/{l.Substring 5}", files
         | l when l.StartsWith "$ cd"               -> $"{path}/{l.Substring 5}", files
-        | l when l.[0] |> System.Char.IsDigit      -> path, (path, l |> fun (i:string) -> i.Split " " |> Seq.head |> int) :: files
-        | l when l.StartsWith "dir"                -> path, (path, 0) :: files
-        | _                                        -> path, files
+        | l when l.[0] |> Char.IsDigit             -> path, (path, l |> Seq.takeWhile Char.IsDigit |> Array.ofSeq |> String |> int) :: files
+        | _                                        -> path, (path, 0) :: files
         ||> calculateSizes tail
 
-let terminalOutput = System.IO.File.ReadLines "inputs/day07.txt" |> Seq.tail |> Seq.toList
+let terminalOutput = IO.File.ReadLines "inputs/day07.txt" |> Seq.tail |> Seq.toList
 let sizes = calculateSizes terminalOutput "/" []
-let spaceNeeded = sizes |> List.find (fst >> (=) "/") |> fun (_, spaceUsed) -> 30000000 - (70000000 - spaceUsed)
+let spaceNeeded = sizes |> Map.find "/" |> fun size -> 30000000 - (70000000 - size)
 
-printfn "Part 1: %i" (sizes |> List.where (snd >> (>=) 100000) |> List.sumBy snd)
-printfn "Part 2: %A" (sizes |> List.sortBy snd |> List.find (snd >> (<) spaceNeeded))
+printfn "Part 1: %i" (sizes |> Map.values |> Seq.where ((>=) 100000) |> Seq.sum)
+printfn "Part 2: %A" (sizes |> Map.values |> Seq.sort |> Seq.find ((<) spaceNeeded))
