cba3c82d94923b33f9027e2955595529187cbdd8
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-10 12:17:07 +0200

    Day 8 fix typo

--- day08.fsx
+++ day08.fsx
@@ -6,7 +6,7 @@ let rec calculateScore (score : int) (tree : int) (line : int list) =
     | h :: _ when tree = h || tree < h -> score + 1
     | _ :: t -> calculateScore (score + 1) tree t
 
-let rec checkVisiblityAndScore (map : (bool * int) list) (trees : int list) =
+let rec checkVisibilityAndScore (map : (bool * int) list) (trees : int list) =
     match map.Length with
     | i when i = trees.Length -> map
     | i ->
@@ -14,10 +14,10 @@ let rec checkVisiblityAndScore (map : (bool * int) list) (trees : int list) =
         let visibilityLeft = trees.[i+1..] |> List.forall ((>) trees.[i])
         let scoreRight = trees.[..i-1] |> List.rev |> calculateScore 0 trees.[i]
         let scoreLeft = trees.[i+1..] |> calculateScore 0 trees.[i]
-        checkVisiblityAndScore (map @ [visibilityRight || visibilityLeft, scoreRight * scoreLeft]) trees
+        checkVisibilityAndScore (map @ [visibilityRight || visibilityLeft, scoreRight * scoreLeft]) trees
 
-let horizontal = forest |> List.map (checkVisiblityAndScore [])
-let vertical = forest |> List.transpose |> List.map (checkVisiblityAndScore []) |> List.transpose
+let horizontal = forest |> List.map (checkVisibilityAndScore [])
+let vertical = forest |> List.transpose |> List.map (checkVisibilityAndScore []) |> List.transpose
 let result = (horizontal, vertical) ||> List.map2 (List.map2 (fun (v1,s1) (v2,s2) -> (v1 || v2), (s1 * s2) ))
 
 printfn "Part 1: %i" (result |> List.sumBy(List.where fst >> List.length))
