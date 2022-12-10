32d16f9186b740b3de58279c070a51f5e61f75db
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-10 11:45:57 +0200

    Day 8 cleanup

--- day08.fsx
+++ day08.fsx
@@ -1,26 +1,22 @@
 let forest = System.IO.File.ReadAllLines "inputs/day08.txt" |> Array.map (Seq.map (System.Char.ToString >> int) >> Seq.toList) |> List.ofArray
 
-let rec checkVisibility (visibilityMap : (bool * int) list) (treeLine : int list) =
-    if visibilityMap.Length = treeLine.Length then 
-        visibilityMap |> List.rev
-    else 
-        let i = visibilityMap.Length
-        let right = treeLine.[..i-1] |> List.forall ((>) treeLine.[i])
-        let left = treeLine.[i+1..] |> List.forall ((>) treeLine.[i])
-        let rec getScore (score : int) (tree : int) (line : int list) =
-            match line with
-            | [] -> score
-            | h :: _ when tree = h || tree < h -> score + 1
-            | _ :: t -> getScore (score + 1) tree t
+let rec calculateScore (score : int) (tree : int) (line : int list) =
+    match line with
+    | [] -> score
+    | h :: _ when tree = h || tree < h -> score + 1
+    | _ :: t -> calculateScore (score + 1) tree t
 
-        let rightScore = treeLine.[..i-1] |> List.rev |> getScore 0 treeLine.[i]
-        let leftScore = treeLine.[i+1..] |> getScore 0 treeLine.[i]
+let rec checkVisiblityAndScore (map : (bool * int) list) (trees : int list) =
+    match map.Length with
+    | i when i = trees.Length -> map
+    | i ->
+        let visibility = trees.[..i-1] |> List.forall ((>) trees.[i]) || trees.[i+1..] |> List.forall ((>) trees.[i])
+        let score = (trees.[..i-1] |> List.rev |> calculateScore 0 trees.[i]) * (trees.[i+1..] |> calculateScore 0 trees.[i])
+        checkVisiblityAndScore (map @ [visibility, score]) trees
 
-        checkVisibility ((right || left, rightScore * leftScore) :: visibilityMap) treeLine
+let horizontal = forest |> List.map (checkVisiblityAndScore [])
+let vertical = forest |> List.transpose |> List.map (checkVisiblityAndScore []) |> List.transpose
+let result = (horizontal, vertical) ||> List.map2 (List.map2 (fun (v1,s1) (v2,s2) -> (v1 || v2), (s1 * s2) ))
 
-let horizontal = forest |> List.map (checkVisibility [])
-let vertical = forest |> List.transpose |> List.map (checkVisibility []) |> List.transpose
-let result = (horizontal, vertical) ||> List.map2 (List.map2 (fun (x,y) (a,b) -> (x || a), (y * b) ))
-
-printfn "Part 1: %i" (result |> List.sumBy(List.where (fst >> id) >> List.length))
-printfn "Part 2: %i" (result |> List.concat |> List.map snd |> List.max)
+printfn "Part 1: %i" (result |> List.sumBy(List.where fst >> List.length))
+printfn "Part 2: %A" (result |> List.concat |> List.maxBy snd)
