9ed650f93a2677cda5f508ccd5b8178ed931caba
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-10 10:55:22 +0200

    Day 8 cleanup

--- day08.fsx
+++ day08.fsx
@@ -5,7 +5,7 @@ let rec checkVisibility (visibilityMap : (bool * int) list) (treeLine : int list
         visibilityMap |> List.rev
     else 
         let i = visibilityMap.Length
-        let right = treeLine.[ .. i-1] |> List.forall ((>) treeLine.[i])
+        let right = treeLine.[..i-1] |> List.forall ((>) treeLine.[i])
         let left = treeLine.[i+1..] |> List.forall ((>) treeLine.[i])
         let rec getScore (score : int) (tree : int) (line : int list) =
             match line with
@@ -20,5 +20,7 @@ let rec checkVisibility (visibilityMap : (bool * int) list) (treeLine : int list
 
 let horizontal = forest |> List.map (checkVisibility [])
 let vertical = forest |> List.transpose |> List.map (checkVisibility []) |> List.transpose
-(horizontal, vertical) ||> List.map2 (List.map2 (fun (x,y) (a,b) -> (x || a), (y * b) )) |> List.sumBy(List.where (fst >> id) >> List.length) |> printfn "Part 1: %i"
-(horizontal, vertical) ||> List.map2 (List.map2 (fun (x,y) (a,b) -> (x || a), (y * b) )) |> List.concat |> List.map snd |> List.max |> printfn "Part 2: %i"
+let result = (horizontal, vertical) ||> List.map2 (List.map2 (fun (x,y) (a,b) -> (x || a), (y * b) ))
+
+printfn "Part 1: %i" (result |> List.sumBy(List.where (fst >> id) >> List.length))
+printfn "Part 2: %i" (result |> List.concat |> List.map snd |> List.max)
