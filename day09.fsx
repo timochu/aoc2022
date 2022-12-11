4f91f1b18f4314be39ead159d74134caa1298aee
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-11 19:07:59 +0200

    Day 9 cleanup

--- day09.fsx
+++ day09.fsx
@@ -13,11 +13,8 @@ let tail (coordinates : (int * int) list) =
     ([(0,0)], coordinates) ||> List.fold (fun acc (hx, hy) -> 
         let tx, ty = acc.Head
         let dx, dy = hx - tx, hy - ty
-        let distance = max (hx - tx |> abs) (hy - ty |> abs)
-        if distance < 2 then acc.Head :: acc
-        else (tx + (compare dx 0), ty + (compare dy 0)) :: acc
-
-    ) |> List.rev
+        if max (hx - tx |> abs) (hy - ty |> abs) < 2 then acc.Head :: acc
+        else (tx + (compare dx 0), ty + (compare dy 0)) :: acc) |> List.rev
 
 let input = System.IO.File.ReadAllLines "inputs/day09.txt"
 printfn "Part 1: %i" (input |> head |> tail |> List.distinct |> List.length)
