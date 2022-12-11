f2fb6e3ef224ac94cb6403d7c8435814bf2b6a3f
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-11 18:30:46 +0200

    Day 9 cleanup

--- day09.fsx
+++ day09.fsx
@@ -1,25 +1,17 @@
-let rec solveStep (coords : (int * int) list) operation  magnitude = 
-    if magnitude = 0 then coords
-    else solveStep (coords @ [coords |> List.last |> operation]) operation (magnitude-1)
-
-let rec headSolver (coordinates : (int * int) list) (motions : string list) =
-    match motions with
-    | [] -> coordinates
-    | motion :: tail -> 
-        let operation, magnitude =
+let headSolver (motions : string list) =
+    ([(0,0)], motions) ||> List.fold (fun coordinates motion ->
+        let operation, distance =
             match motion.Split " " with
             | [|"U"; i|] -> (fun (x,y) -> (x,y+1)), (int i)
             | [|"D"; i|] -> (fun (x,y) -> (x,y-1)), (int i)
             | [|"L"; i|] -> (fun (x,y) -> (x-1,y)), (int i)
             | [|"R"; i|] -> (fun (x,y) -> (x+1,y)), (int i)
-        let coords = solveStep [coordinates |> List.last] operation magnitude |> List.skip 1
-        headSolver (coordinates @ coords) tail
+        let coords = ([coordinates.Head], [1 .. distance]) ||> List.fold (fun acc _ -> (acc.Head |> operation) :: acc) |> List.rev |> List.tail |> List.rev
+        coords @ coordinates) |> List.rev
 
-
-let rec tailSolver (coords : (int * int) list) (headCoords : (int * int) list) = 
-    match headCoords, coords with
-    | [], _ -> coords |> List.rev
-    | (hx,hy) :: t, (tx, ty) :: _ ->
+let rec tailSolver (headCoords : (int * int) list) = 
+    ([(0,0)], headCoords) ||> List.fold (fun coords (hx, hy) -> 
+        let (tx, ty) = coords |> List.head
         let x, y = hx - tx, hy - ty
         let distance = max (hx - tx |> abs) (hy - ty |> abs)
         let newPosition = 
@@ -28,22 +20,23 @@ let rec tailSolver (coords : (int * int) list) (headCoords : (int * int) list) =
                 let xop = if x > 0 then +1 elif x < 0 then -1 else 0
                 let yop = if y > 0 then +1 elif y < 0 then -1 else 0
                 (tx+xop, ty+yop)
-        tailSolver (newPosition :: coords) t
+        (newPosition :: coords)
+    ) |> List.rev
 
 let input = System.IO.File.ReadAllLines "inputs/day09.txt" |> Array.toList
-let headCoords = headSolver [(0,0)] input
+let headCoords = headSolver input
 
-printfn "Part 1: %i" (headCoords |> tailSolver [(0,0)] |> List.distinct |> List.length)
+printfn "Part 1: %i" (headCoords |> tailSolver |> List.distinct |> List.length)
 printfn "Part 2: %i" (
     headCoords 
-    |> tailSolver [(0,0)] 
-    |> tailSolver [(0,0)] 
-    |> tailSolver [(0,0)] 
-    |> tailSolver [(0,0)] 
-    |> tailSolver [(0,0)] 
-    |> tailSolver [(0,0)] 
-    |> tailSolver [(0,0)] 
-    |> tailSolver [(0,0)] 
-    |> tailSolver [(0,0)] 
+    |> tailSolver
+    |> tailSolver
+    |> tailSolver
+    |> tailSolver
+    |> tailSolver
+    |> tailSolver
+    |> tailSolver
+    |> tailSolver
+    |> tailSolver
     |> List.distinct 
     |> List.length)
