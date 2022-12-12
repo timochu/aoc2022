86470b7d4d05fd81d6514cbe488a6d934a8910d9
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-12 14:32:24 +0200

    Day 10 part 1 cleanup

--- day10.fsx
+++ day10.fsx
@@ -1,9 +1,9 @@
 System.IO.File.ReadLines "inputs/day10.txt"
-|> Seq.fold(fun (acc : (int * int) list) (line:string) ->
+|> Seq.fold (fun (acc : (int * int) list) (line:string) ->
     let cycle, register = acc |> List.head
     match line[..4] with
     | "noop" -> (cycle+1, register) :: acc
     | _ -> (cycle+2, register + int line[5..]) :: (cycle+1, register) :: acc) [1,1]
-|> Seq.choose(fun (x,y) -> if x=20 || x=60 || x=100 || x=140 || x=180 || x=220 then Some(x*y) else None )
+|> Seq.choose (fun (c,r) -> if List.contains c [20;60;100;140;180;220] then Some(c * r) else None)
 |> Seq.sum
 |> printfn "Part 1: %A"
