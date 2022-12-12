a0f139dbda9525796ac7f345b30122ea9adbee46
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-12 09:29:51 +0200

    Day 9 cleanup

--- day09.fsx
+++ day09.fsx
@@ -1,18 +1,18 @@
 let head =
     Seq.fold (fun acc (direction, distance) ->
-        let folder (acc : list<int * int>) _ =
-            match direction, acc.Head with
-            | 'U', (x,y) -> (x, y + 1) :: acc
-            | 'D', (x,y) -> (x, y - 1) :: acc
-            | 'L', (x,y) -> (x - 1, y) :: acc
-            | _  , (x,y) -> (x + 1, y) :: acc
+        let folder acc _ =
+            match direction, List.head acc with 
+            | 'U', (x,y) -> (x,y+1) :: acc
+            | 'D', (x,y) -> (x,y-1) :: acc
+            | 'L', (x,y) -> (x-1,y) :: acc
+            |  _ , (x,y) -> (x+1,y) :: acc
         List.fold folder [acc.Head] [1 .. distance] @ acc) [0,0] >> List.rev
 
 let tail = 
     List.fold (fun acc (hx, hy) -> 
         let tx, ty = List.head acc
         let dx, dy = hx - tx, hy - ty
-        if max (abs(hx-tx)) (abs(hy-ty)) < 2 then acc.Head :: acc
+        if max (abs(hx-tx)) (abs(hy-ty)) < 2 then acc
         else (tx + (compare dx 0), ty + (compare dy 0)) :: acc) [0,0] >> List.rev
 
 let input = System.IO.File.ReadLines "inputs/day09.txt" |> Seq.map (fun s -> s[0], int s[2..])
