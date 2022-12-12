a0ab98a3fa1a0e33ca0c2cfe47538a3574fe580c
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-12 10:49:38 +0200

    Day 9 cleanup

--- day09.fsx
+++ day09.fsx
@@ -1,7 +1,8 @@
+#time
 let head =
     Seq.fold (fun acc (direction, distance) ->
         List.fold (fun acc i ->
-            let x, y = acc |> List.last
+            let x, y = List.last acc
             match direction with | 'U' -> x,y+i | 'D' -> x,y-i | 'L' -> x-i,y | _ -> x+i,y
             :: acc) [acc[0]] [1 .. distance] @ acc) [0,0] >> List.rev
 
@@ -12,7 +13,7 @@ let tail =
         if max (abs(hx-tx)) (abs(hy-ty)) < 2 then acc
         else (tx+(compare dx 0), ty+(compare dy 0)) :: acc) [0,0] >> List.rev
 
-let input = System.IO.File.ReadLines "inputs/day09.txt" |> Seq.map (fun s -> s[0], int s[2..])
+let input = System.IO.File.ReadLines "inputs/day09.txt" |> Seq.map (fun s -> s[0], int s[2..]) |> head |> tail
 
-printfn "Part 1: %i" (input |> head |> tail |> List.distinct |> List.length)
-printfn "Part 2: %i" (input |> head |> tail |> tail |> tail |> tail |> tail |> tail |> tail |> tail |> tail |> List.distinct |> List.length)
+printfn "Part 1: %i" (input |> List.distinct |> List.length)
+printfn "Part 2: %i" (input |> tail |> tail |> tail |> tail |> tail |> tail |> tail |> tail |> List.distinct |> List.length)
