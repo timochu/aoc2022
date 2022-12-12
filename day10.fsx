1b80aabb429a837b6cd1ea3f5c64e916ee45cad9
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-12 14:27:21 +0200

    Day 10 part 1 cleanup

--- day10.fsx
+++ day10.fsx
@@ -4,13 +4,6 @@ System.IO.File.ReadLines "inputs/day10.txt"
     match line[..4] with
     | "noop" -> (cycle+1, register) :: acc
     | _ -> (cycle+2, register + int line[5..]) :: (cycle+1, register) :: acc) [1,1]
-|> fun x -> [
-    x |> List.find (fst >> (=) 20)
-    x |> List.find (fst >> (=) 60)
-    x |> List.find (fst >> (=) 100)
-    x |> List.find (fst >> (=) 140)
-    x |> List.find (fst >> (=) 180)
-    x |> List.find (fst >> (=) 220)]
-|> List.map (fun (a, b) -> a * b)
-|> List.sum
+|> Seq.choose(fun (x,y) -> if x=20 || x=60 || x=100 || x=140 || x=180 || x=220 then Some(x*y) else None )
+|> Seq.sum
 |> printfn "Part 1: %A"
