329811423a7996b397506d8725d64c5ad6fa1916
Timo Vahtera <timo.vahtera@solita.fi>
2022-12-12 12:09:20 +0200

    Day 9 cleanup

--- day09.fsx
+++ day09.fsx
@@ -10,7 +10,7 @@ let tail =
     List.fold (fun acc (hx, hy) -> 
         let tx, ty = List.head acc
         let dx, dy = hx-tx, hy-ty
-        if max(abs(hx-tx)) (abs(hy-ty)) < 2 then acc
+        if max(abs(dx)) (abs(dy)) < 2 then acc
         else (tx+(compare dx 0), ty+(compare dy 0)) :: acc) [0,0] >> List.rev
 
 let input = System.IO.File.ReadLines "inputs/day09.txt" |> Seq.map (fun s -> s[0], int s[2..]) |> head |> tail
