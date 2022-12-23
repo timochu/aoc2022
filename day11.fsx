3c1b03e707a2c91e0ef6a17adff8d1502568bd1c
Timo Vahtera <timo.vahtera@gmail.com>
2022-12-23 18:54:50 +0200

    Day 11 cleanup

--- day11.fsx
+++ day11.fsx
@@ -1,11 +1,11 @@
 open System.Collections.Generic
 
 let input, lines = System.IO.File.ReadAllLines "inputs/day11.txt" |> fun input -> input, input.Length
-let items, items2 = input |> fun s -> [for i in 1 .. 7 .. lines -> (s[i][18..]).Split ", " |> Array.map int64 |> fun x -> Queue x, Queue x] |> List.unzip
-let operations    = input |> fun s -> [for i in 2 .. 7 .. lines -> s[i][19..]]
-let tests         = input |> fun s -> [for i in 3 .. 7 .. lines -> s[i][21..] |> int64]
-let trues         = input |> fun s -> [for i in 4 .. 7 .. lines -> s[i][29..] |> int]
-let falses        = input |> fun s -> [for i in 5 .. 7 .. lines -> s[i][29..] |> int]
+let items1, items2 = input |> fun s -> [for i in 1 .. 7 .. lines -> (s[i][18..]).Split ", " |> Array.map int64 |> fun x -> Queue x, Queue x] |> List.unzip
+let operations     = input |> fun s -> [for i in 2 .. 7 .. lines -> s[i][19..]]
+let tests          = input |> fun s -> [for i in 3 .. 7 .. lines -> s[i][21..] |> int64]
+let trues          = input |> fun s -> [for i in 4 .. 7 .. lines -> s[i][29..] |> int]
+let falses         = input |> fun s -> [for i in 5 .. 7 .. lines -> s[i][29..] |> int]
 
 let compute rounds worryReducer (items : int64 Queue list) =
     let inspections = Array.zeroCreate items.Length
@@ -23,5 +23,5 @@ let compute rounds worryReducer (items : int64 Queue list) =
                 inspections[monkey] <- inspections[monkey] + 1UL )
     inspections |> Array.sortDescending |> Array.take 2 |> Array.reduce (*)
 
-printfn "Part 1: %i" (items |> compute 20 (fun  worry -> worry / 3L))
-printfn "Part 2: %i" (items2 |> compute 10000 (fun  worry -> worry % (tests |> List.reduce (*))))
+printfn "Part 1: %i" (items1 |> compute 20 (fun worry -> worry / 3L))
+printfn "Part 2: %i" (items2 |> compute 10000 (fun worry -> worry % List.reduce (*) tests))
